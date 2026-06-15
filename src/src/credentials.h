34456826a026a9b9221a1d468fcbe67e1d69363d
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2026-06-15 23:37:05 +0100

    Add credentials.h for network and MQTT configuration

---
+++ src/src/credentials.h
@@ -0,0 +1,14 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// Network Configuration Definitions
+constexpr const char* WIFI_SSID     = "152 2.4GHz";
+constexpr const char* WIFI_PASSWORD = "derwenthorpe";
+
+// Broker Endpoint Profiles
+constexpr const char* MQTT_SERVER   = "robotmqtt";
+constexpr const char* MQTT_CLIENTID = "halloween2";
+constexpr const char* MQTT_USERNAME = "public";
+constexpr const char* MQTT_KEY      = "public";
+
+#endif
